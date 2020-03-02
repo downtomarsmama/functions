42af9558e12f27636ac10e0c19e7224a44b559dc
downtomarsmama <54377186+downtomarsmama@users.noreply.github.com>
2020-03-02 10:34:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+
+//functions
+function sayHelloWorld(){
+    console.log("Hello, world!");
+    console.log("Hola, Tacos por favor")
+  }
+  sayHelloWorld();
+  sayHelloWorld();
+
+  function ilovejavascript(){
+      console.log("I love JavaScript!")
+  }
+  ilovejavascript();
+
+
+
+
+
+var English = true;
+var Spanish = false;
+
+
+
+if(English){
+  console.log("Hello,Kayla!");
+}
+
+
+
+if(Spanish){
+  console.log("Hola, Kayla!");
+}
+
+
+function add(x, y) {
+    return x+y;
+}
+console.log(add(2, 3));      
+
+function subtract(x, y) {
+    return x-y;
+}
+console.log(subtract(10, 5)); 
+
+function divide(x, y) {
+    return x/y;
+}
+console.log(divide(4, 4));     
+
+function multiply(x, y) {
+    return x*y;
+}
+console.log(multiply(2, 3));    
+
+
+function makeSandwich(breadType,sandwichName,isToasted){
+    var finishedSandwich = `You ordered A ${isToasted} ${sandwichName} sandwich on ${breadType} `
+    return  finishedSandwich;
+}
+var mySandwich = makeSandwich("wheat", "meatball sub", "toasted");
+console.log(mySandwich);
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+    
